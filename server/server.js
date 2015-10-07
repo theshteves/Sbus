af408277f8c39e26ea694318a7e1a2924b2d5330
theshteves <kneisers@msu.edu>
2015-10-06 20:05:29 -0400

    quicksave

--- server/server.js
+++ server/server.js
@@ -1,7 +1,28 @@
-var express = require('express'),
+var chalk = require('chalk'),
+    GtfsRealtimeBindings = require('gtfs-realtime-bindings'),
+    express = require('express'),
     path = require('path'),
-    chalk = require('chalk'),
+    request = require('request'),
     app = express();
+
+var requestSettings = {
+    method: 'GET',
+    url: 'URL OF YOUR GTFS-REALTIME SOURCE GOES HERE',
+    encoding: null
+};
+
+/*
+request(requestSettings, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+	var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+	feed.entity.forEach(function(entity) {
+	    if (entity.trip_update) {
+		console.log(entity.trip_update);
+	    }
+	});
+    }
+});
+*/
 
 app.use(express.static(path.join(__dirname, '../public')))
 
